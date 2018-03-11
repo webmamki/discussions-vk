e984e9953e22f5345406b0c1ec4a8ab75dec0591
webmamki <webmanki@gmail.com>
2018-03-11 21:03:53 +0300

    Update d_reviews.php

--- build/d_reviews.php
+++ build/d_reviews.php
@@ -8,22 +8,22 @@
 	$version = "5.4"; // Версия VK API (На текущий момент менять не нужно)
 
 
-	$page = file_get_contents("https://api.vk.com/method/board.getComments?" . "group_id=" . $group_id . "&topic_id=" . $topic_id . "&count=" . $count . "&extended=" . $extended . "&need_likes=" . $need_likes . "&sort=" . $sort . "&v=" . $version);
+	//$page = file_get_contents("https://api.vk.com/method/board.getComments?" . "group_id=" . $group_id . "&topic_id=" . $topic_id . "&count=" . $count . "&extended=" . $extended . "&need_likes=" . $need_likes . "&sort=" . $sort . "&v=" . $version);
 
-	echo $page;
+	//echo $page;
 
 	/* Если не срабатывает php код и страница с JSON пустая, то потребуется закомментировать $page и echo $page и раскомментировать код ниже */
 
-	// $page = "https://api.vk.com/method/board.getComments?" . "group_id=" . $group_id . "&topic_id=" . $topic_id . "&count=" . $count . "&extended=" . $extended . "&need_likes=" . $need_likes . "&sort=" . $sort . "&v=" . $version";
+	 $page = "https://api.vk.com/method/board.getComments?" . "group_id=" . $group_id . "&topic_id=" . $topic_id . "&count=" . $count . "&extended=" . $extended . "&need_likes=" . $need_likes . "&sort=" . $sort . "&v=" . $version";
 
-	// function file_get_contents_curl($url) {
-  //   $ch = curl_init();
-  //   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  //   curl_setopt($ch, CURLOPT_URL, $url);
-  //   $data = curl_exec($ch);
-  //   curl_close($ch);
-  //   return $data;
-	// }
-	//
-	// echo file_get_contents_curl($page);
+function file_get_contents_curl($url) {
+  $ch = curl_init();
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_URL, $url);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
+echo file_get_contents_curl($page);
 ?>
